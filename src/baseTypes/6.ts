3099072c367ac045afd98be1f40614819aa258bf
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-09-23 22:33:31 +0300

    add 1-7tasks

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,11 +2,11 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
+function showMessage(message: string) {
   console.log(message);
 }
 
-function calc(num1, num2) {
+function calc(num1: number, num2: number) {
   return num1 + num2;
 }
 
